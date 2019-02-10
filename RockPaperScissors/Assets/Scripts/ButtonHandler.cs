fe35bed11cc14999db8461ed621d73c47727d1a0
Constantin Cezar Begu <38817297+ConstantinCezB@users.noreply.github.com>
2019-02-10 15:29:39 -0500

    Created the menu scene

---
+++ RockPaperScissors/Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    public void playButton ()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void exitButton()
+    {
+        Application.Quit();
+    }
+
+}
