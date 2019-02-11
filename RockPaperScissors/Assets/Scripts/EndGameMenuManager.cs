5b77defe495854056ffe6e5c442c1bd0432b247d
Constantin Cezar Begu <38817297+ConstantinCezB@users.noreply.github.com>
2019-02-10 21:11:04 -0500

    endGame scene is completed. Need to make it look more nice and clean the code

--- RockPaperScissors/Assets/Scripts/EndGameMenuManager.cs
+++ RockPaperScissors/Assets/Scripts/EndGameMenuManager.cs
@@ -1,18 +1,35 @@
-﻿using System.Collections;
+﻿
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class EndGameMenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject endGameText;
+
     void Start()
     {
-        
+        if (ApplicationModel.winner == 0)
+        {
+            endGameText.GetComponent<Text>().text = "The Game ended with a draw.\nBetter luck next time!";
+        }
+        else if (ApplicationModel.winner == 2)
+        {
+            endGameText.GetComponent<Text>().text = "You lost to the computer.\nCan't really congratulate you on that!";
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    // Start is called before the first frame update
+
+    public void playButton()
     {
-        
+        SceneManager.LoadScene("Game");
     }
+
+    public void exitButton()
+    {
+        Application.Quit();
+    }
+
 }
